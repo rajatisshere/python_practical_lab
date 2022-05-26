6b1d31af7b038ede5cd671de9ed908b7e993a752
prabhatishere <106159137+prabhatishere@users.noreply.github.com>
2022-05-26 17:25:22 +0530

    Add files via upload

---
+++ Dictionary/09_dict.py
@@ -0,0 +1,7 @@
+# compairing two  dictionaries
+dict1 = eval(input("enter the input in first dictionary form "))
+dict2 = eval(input("enter the input in second dictionary form "))
+if dict1 == dict2:
+    print("your dictionarries are equal ")
+else:
+    print("your dictionaries are not equal")
