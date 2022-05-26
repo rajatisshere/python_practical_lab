49b47663f02994604254b1cef599352e4d164c52
prabhatishere <106159137+prabhatishere@users.noreply.github.com>
2022-05-26 17:16:35 +0530

    Add files via upload

---
+++ Dictionary/07_dict.py
@@ -0,0 +1,7 @@
+# program to get the keys of a dictionary
+dict = eval(input("enter the input in dicitionary form "))
+print("your given dictionary is given below")
+print(dict)
+print("keys from your dictionary are given below")
+for i in dict.keys:
+    print(i)
