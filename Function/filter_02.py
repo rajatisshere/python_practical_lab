569af48b0210c1fbc8a3137028109b361c052327
prabhatishere <106159137+prabhatishere@users.noreply.github.com>
2022-05-26 17:36:27 +0530

    Add files via upload

---
+++ Function/filter_02.py
@@ -0,0 +1,10 @@
+def marks(k):
+    if k>70 and k<90:
+        return marks
+l=eval(input("enter the marks of student in list manner"))
+g="grdfghfgarawedra"
+s=list(filter(marks,l))
+print(s)
+print(l.sort())
+m=g.sort()
+print(m)
