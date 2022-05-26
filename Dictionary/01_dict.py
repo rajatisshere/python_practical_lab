e551cb00f2ef077e1778cbc4ebb9fcdb2a26083b
prabhatishere <106159137+prabhatishere@users.noreply.github.com>
2022-05-26 17:13:06 +0530

    Add files via upload

---
+++ Dictionary/01_dict.py
@@ -0,0 +1,7 @@
+''' create a dictionary students containing names and ages.copying the dictionaries into stud.Empty the students
+ dictionsry , as stud continues to hold the data '''
+students = eval(input("enter the dictionary input,containing names and ages "))
+stud = students
+students = {}
+print(stud)
+    
