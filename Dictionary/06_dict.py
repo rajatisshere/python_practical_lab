4f75897fe7fa03d3cd3d5608966dbce897e16f41
prabhatishere <106159137+prabhatishere@users.noreply.github.com>
2022-05-26 17:16:17 +0530

    Add files via upload

---
+++ Dictionary/06_dict.py
@@ -0,0 +1,9 @@
+sample_dict = {
+    "Topper": "Shivam",
+    "Second": "Kapil",
+    "Third": "Lovekesh",
+    "Fourth": "Prabhat"
+}
+
+sample_dict['First'] = sample_dict.pop('Fourth')
+print(sample_dict)
