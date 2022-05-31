a9c1d09ac52e93363ba469fbb0a3c48fd1ded0e3
prabhatishere <106159137+prabhatishere@users.noreply.github.com>
2022-05-31 08:38:14 +0530

    Update Q10.py

--- NUmpy/Q10.py
+++ NUmpy/Q10.py
@@ -1,1 +1,16 @@
 
+import numpy as np
+
+q=np.array(([1,2,3,4,5],[6,7,8,9,10]))
+
+print(q)
+
+f=np.ndarray.flatten(q)
+
+print("the flattened array is ",f)
+
+max=f.max()
+
+min=f.min()
+
+print("the max and min value of flattened
